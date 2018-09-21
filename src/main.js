71f87d8311f8e869b65305d97b2f3194819f4f71
INANI EL HOUSSAIN <inanielhoussain@gmail.com>
2018-09-21 22:14:20 +0100

    Set up routes

--- src/main.js
+++ src/main.js
@@ -1,8 +1,23 @@
 import Vue from 'vue'
 import App from './App.vue'
 
+import VueRouter from 'vue-router'
+
+
+// Set the Vue use the router
+Vue.use(VueRouter)
+
+import {routes} from './routes'
+
+
+const router = new VueRouter({
+	routes,
+	mode: 'history'
+})
+
 Vue.config.productionTip = false
 
 new Vue({
-  render: h => h(App)
+  render: h => h(App),
+  router
 }).$mount('#app')
---
+++ src/routes.js
@@ -0,0 +1,9 @@
+// the components
+import HelloWorld from './components/HelloWorld.vue'
+import User from './components/User.vue'
+
+// Set the routes
+export const routes =[
+	{ path: '/', component: HelloWorld },
+	{ path: '/user', component: User }
+]
